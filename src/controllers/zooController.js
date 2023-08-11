59dc0e037e1d846b80a8d6f5d7a5824cfff209a7
Jhumberto <jhumbertto@gmail.com>
2023-08-10 18:02:30 -0600

    Adding Routes and Controllers

--- src/controllers/zooController.js
+++ src/controllers/zooController.js
@@ -1,4 +1,5 @@
 import { Zoo } from "../models/Zoo.js"
+import { Op } from "sequelize";
 
 export const getAllZoo = async (req,res) =>{
   let response;
@@ -8,5 +9,90 @@ export const getAllZoo = async (req,res) =>{
   }catch(e){
     res.status(500).json({"Error": e.message})
   }
-  
+};
+
+export const getOneZoo = async (req,res) =>{
+  const {id} = req.params;
+  let response;
+  try {
+    response = await Zoo.findByPk(id);
+    res.status(200).json(response)
+  } catch (e) {
+    res.status(500).json({"Error": e.message})
+    return;
+  }
+  if(response === null){
+    res.status(404).json({
+      "error": "Tu registro no se encuentra en la base de datos"
+    })
+    return;
+  }
+}
+
+export const getZooBudget = async (req,res) =>{
+  let response;
+
+  try {
+    response = await Zoo.findAll({
+      attributes:['zoo_name', 'budget'], order:[['budget', 'DESC']]
+    });
+    res.status(200).json(response)
+  } catch (e) {
+    res.status(500).json({"Error": e.message})
+    return;
+  }
+}
+
+export const getZooNombre = async (req,res) =>{
+  const {nombre} =  req.body;
+  let response;
+  let sentenciaLike = "%"+nombre.toLowerCase()+"%"
+  try {
+    response = await Zoo.findAll({
+      where: {
+        'zoo_name': {[Op.iLike]:sentenciaLike}
+      }
+    });
+    res.status(200).json(response)
+  } catch (e) {
+    res.status(500).json({"Error": e.message})
+    return;
+  }
+  if(response.length === 0){
+    res.status(404).json({
+      "error": "Tu registro no se encuentra en la base de datos"
+    })
+    return;
+  }
+}
+
+export const createZoo = async (req,res) =>{
+  const {zoo_name, city_id, zoo_size, budget} =  req.body;
+  let response; 
+  try {
+    response = await Zoo.create({
+      zoo_name,
+      city_id,
+      zoo_size,
+      budget
+    });
+    res.status(200).json({"Succesful Register": response.dataValues})
+  } catch (e) {
+    res.status(500).json({"Error": e.message})
+    return;
+  }
+}
+
+export const deleteZoo = async (req, res)=>{
+  const{id_zoo} = req.params
+  let response;
+  try {
+    response = await Zoo.destroy({
+      where:{id_zoo}
+    })
+    res.status(200).json({"Estado Transaccion": response})
+  } catch (e) {
+    res.status(500).json({"Error": e.message})
+    return;
+  }
 }
--- src/routes/zooRoutes.js
+++ src/routes/zooRoutes.js
@@ -1,12 +1,13 @@
 import { Router, application } from "express";
-import { getAllZoo } from "../controllers/zooController.js";
+import { getAllZoo, getOneZoo, getZooBudget, getZooNombre, createZoo, deleteZoo } from "../controllers/zooController.js";
 
 const router = Router()
 
 router.get('/api/zoo', getAllZoo)
-// router.length('/api/zoo/id/:id', getOneZoo)
-// router.length('/api/zoo/budget', getZooBudget)
-// router.length('/api/zoo/findByName', getZooNombre)
-// router.length('/api/zoo/createZoo', createZoo)
+router.get('/api/zoo/id/:id', getOneZoo)
+router.get('/api/zoo/budget', getZooBudget)
+router.post('/api/zoo/findByName', getZooNombre)
+router.post('/api/zoo/createZoo', createZoo)
+router.delete('/api/zoo/deleteZoo/:id_zoo', deleteZoo)
 
 export default router;
